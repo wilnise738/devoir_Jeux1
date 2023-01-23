4ec00a2816b52c97f8c5d67b762b0d3f81c600f1
wilnise738 <119763265+wilnise738@users.noreply.github.com>
2023-01-22 20:39:44 -0500

    Update jeux1.py

--- jeux1.py
+++ jeux1.py
@@ -1,37 +1,20 @@
 from random import randrange
-
-comp_nommber=randrange(0,500)
-chance = 5
-
-print("Bienvenue dans mon jeux! \nCe jeux, tu vas deviner un nombre secret. \n")
-chosen_number=int(input("Entrer un nombre compris entre 0 à 500: \n"))
-
-if chosen_number == comp_nommber:
-    print("Félicitation, tu as gagné")
-
-while chosen_number !=comp_nommber and chance !=0:
-     if chosen_number > comp_nommber:
-        print("Desolée, le nombre secret est inférieur au nombre que tu as entré.")
-        if chance >1:
-            print("Il te reste" ,chance, "chances")
-        elif chance == 1:
-            print("Il te reste" ,chance, "chance")
-        chosen_number=int(input("Entrer donc un autre nombre. \n"))
-     elif chosen_number < comp_nommber:
-        print("Desolée, le nombre secret est supérieur au nombre que tu as entré.")
-        if chance >1:
-            print("Il te reste" ,chance, "chances")
-        elif chance == 1:
-            print("Il te reste" ,chance, "chance")
-        chosen_number=int(input("Entrer donc un autre nombre. \n"))
-     else:
-        print("Felicitation, tu as gagné!")
-        print("Le nombre secret est bien ",comp_nommber)
+nonb_o=randrange(1,10)
+chans =4
+genyen =False
+nonb_c=int(input("Antre yon nonb ant 1 a 10: \n"))
+if nonb_c == nonb_o:
+    print("Bravo, ou genyen. Nonb la se byen " ,nonb_o, "\n")
+while not genyen and chans != 0:
+    if nonb_c < nonb_o:
+        nonb_c=int(input("Sa ou antre a twò piti, antre yon nonb: \n"))
+    elif nonb_c > nonb_o:
+        nonb_c=int(input("Sa ou antre a twò gran, antre yon nonb: \n"))
+    else:
+        genyen = True
         break
-     chance -= 1   
-
-if chance ==0:
-    print("desolée, Tu as épuisé de toutes tes chances.")
-    print("Le nombre secret est" ,comp_nommber,"\n")
-
-print("Merci d'avoir joué avec moi!")
+    chans-=1
+if genyen:
+    print("Bravo, ou genyen. Nonb la se byen " ,nonb_o, "\n")
+if chans == 0:
+    print("Dezole, ou itilize tout chans ou yo. Nonb ou te dwe devine a se " ,nonb_o, "\n")
